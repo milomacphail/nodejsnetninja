41d3b2428b78b1a3147448de80dca075381a0764
Milo MacPhail <milomacphail@gmail.com>
2018-09-13 21:53:32 +0000

    JSON

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log("The request was made "+ req.url);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', "utf8");
-    myReadStream.pipe(res);
-    res.end("Hey Ninjas");
+    res.writeHead(200, {"Content-Type": "application/json"});
+    var myObj = {
+        name: "Ryu",
+        job: "Ninja",
+        age: 29
+        
+    };
+    res.end(JSON.stringify(myObj));
 });
 
 
