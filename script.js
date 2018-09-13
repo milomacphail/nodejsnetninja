33927ec687d9ec9c1bc7a7c2248784f24d41fb9c
Milo MacPhail <milomacphail@gmail.com>
2018-09-13 17:41:13 +0000

    Lesson 16

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ var http = require('http');
 
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', "utf8");
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt', "utf8");
-
-
-myReadStream.on('data', function(chunk){
-    console.log("new chunk received!:");
-    myWriteStream.write(chunk);
+var server = http.createServer(function(req, res){
+    console.log("The request was made "+ req.url);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', "utf8");
+    myReadStream.pipe(res);
+    res.end("Hey Ninjas");
 });
+
+
+server.listen(3000, "127.0.0.1");
+console.log("Now listening to port 3000");
