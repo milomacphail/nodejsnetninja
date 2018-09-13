d5087292b5390887cac91f459588df54418e4aac
Milo MacPhail <milomacphail@gmail.com>
2018-09-13 22:21:45 +0000

    Basic Routing

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log("The request was made "+ req.url);
-    res.writeHead(200, {"Content-Type": "application/json"});
-    var myObj = {
-        name: "Ryu",
-        job: "Ninja",
-        age: 29
-        
-    };
-    res.end(JSON.stringify(myObj));
+    if (req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream(__dirname + './index.html').pipe(res);
+    } else if (req.url === '/contact'){
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream(__dirname + './contact.html').pipe(res);    
+    } else if (req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+        res.writeHead(200, {"Content-type": "application/json"});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {"Content-type": "text/html"});
+        fs.createReadStream(__dirname + './404.html').pipe(res);
+    }
 });
-
 
 server.listen(80, "127.0.0.1");
 console.log("Now listening to port 80");
