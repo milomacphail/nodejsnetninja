7c3d2bf494146ab126c2f9edf86e8c3640c0c453
Milo MacPhail <milomacphail@gmail.com>
2018-09-13 17:10:47 +0000

    Readable Streams

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res){
-    console.log("The request was made "+ req.url);
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end("Hey Ninjas");
-});
+var fs = require('fs');
 
-server.listen(3000, "127.0.0.1");
-console.log("Now listening to port 3000");
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt', "utf8");
+
+myReadStream.on('data', function(chunk){
+    console.log("new chunk received!:");
+    console.log(chunk);
+});
