14681e8d11906f29a70a56785262ef604b6de478
Milo MacPhail <milomacphail@gmail.com>
2018-09-15 15:47:08 +0000

    Lesson 25

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
     app.get("/", function(req, res){
-    res.send('this is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
     
     app.get("/contact", function(req, res){
-    res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
-    app.get("/profile/:id", function(req, res){
-        res.send("You requested to see the profile of: " + req.params.id);
-    })
+    app.get("/profile/:name", function(req, res){
+        var data = {job: "ninja", age: 29};
+        res.render('profile', {person: req.params.name, data: data});
+    });
 
-app.listen(3000);
+app.listen(process.env.PORT, process.env.IP, 3000);
